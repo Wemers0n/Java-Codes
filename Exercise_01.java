e747643687b60b2ed0a22a7deed8602ed002e566
Wemerson <lince8k@gmail.com>
2023-04-01 12:17:34 -0300

    Exercises in Java

---
+++ Exercise_01.java
@@ -0,0 +1,5 @@
+// 01. Leia uma velocidade em m/s, calcule e escreva esta velocidade em km/h. (Vkm/h = Vm/s * 3.6)
+
+public class Exercise_01 {
+    
+}
---
+++ Exercise_02.java
@@ -0,0 +1,5 @@
+// 02. Leia um valor em horas e um valor em minutos, calcule e escreva o equivalente em minutos.
+
+public class Exercise_02 {
+    
+}
