bb1e500f4e37f24ca23b7b2a1a48a08b1646996f
Wemerson <lince8k@gmail.com>
2023-04-01 12:05:37 -0300

    exercises in java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// class Main {  
+//     public static void main(String args[]) { 
+//       System.out.println("Hello World!"); 
+//     } 
+//   }
+
+class Hello{
+  public static void main(String[] args){
+    System.out.println("\nHello World");
+  }
+}
